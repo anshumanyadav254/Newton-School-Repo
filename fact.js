139ffdaada765c2ee81d81f0245dc2c60c5cb2da
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-03-18 12:43:29 +0530

    Update and rename fact.html to fact.js

---
+++ fact.js
@@ -0,0 +1,12 @@
+            
+		var n = prompt(" Enter the Number to calculate "); 
+	
+		function Factorial(n) { 
+			if (n === 0) { 
+				return 1; 
+			} 
+			else { 
+				return n * Factorial( n - 1 ); 
+			} 
+		} 
+	
