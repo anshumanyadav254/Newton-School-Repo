1c856c024d45251e9d53bc31c86c15c785a65a1a
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-03-18 13:18:45 +0530

    Create search.js
    Finding out if a value is in the array or not

---
+++ search.js
@@ -0,0 +1,7 @@
+function searchByValue(array, value) {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (element === value) return index;
+  }
+  return -1;
+}
